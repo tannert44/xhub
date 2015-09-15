c0d4922ca0f2c9657be443c810bd25d04d9ca282
tannert44 <tannert8155@gmail.com>
2015-09-14 22:10:34 -0500

    Added Login inputs

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var app = angular.module("xhubApp", ["ngRoute", "firebase"]);
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
-      .when('/#', {
+      .when('/', {
         templateUrl: 'partials/login.html',
         controller: 'LoginCtrl'
       });
