ff8c89600d939c184dcf2356a5930d6ffa75caa4
tannert44 <tannert8155@gmail.com>
2015-09-21 09:11:50 -0500

    Started working on create repo

--- app/controllers/CulinaryRepo.js
+++ app/controllers/CulinaryRepo.js
@@ -3,6 +3,14 @@ app.controller( 'CulinaryRepoCtrl', [
   '$firebaseArray',
   '$compile',
   function($scope, $firebaseArray, $compile){
+    //Recipe Array to push to firebase
+    var recipeRepoArr = [];
+    //Recipe Object to be added to Recipe Array
+    var recipeRepoObj = {};
+    //Dynamically adds recipe number
+    var ingredientCounter = 0;
+    //Dynamicall adds utensil number
+    var utensilCounter = 0;
     //Get Firebase reference
     var ref = new Firebase("https://xhub.firebaseio.com/users");
     //creates firebase reference
@@ -13,7 +21,7 @@ app.controller( 'CulinaryRepoCtrl', [
       var nextIngredient = angular.element( document.querySelector( '.container-ingredient' ) );
       console.log(nextIngredient);
       var appendedIngredient = '<div class="input-group">';
-          appendedIngredient += '<input type="text" class="form-control" placeholder="Celery 1/4 Cup">';
+          appendedIngredient += '<input type="text" class="form-control ingredientName" placeholder="Celery 1/4 Cup">';
           appendedIngredient += '<span class="input-group-btn">';
           appendedIngredient += '<button class="btn btn-default" type="button" ng-click="addIngredient()">';
           appendedIngredient += '<span class="glyphicon glyphicon-plus" aria-hidden="true">';
@@ -23,19 +31,42 @@ app.controller( 'CulinaryRepoCtrl', [
         nextIngredient.append(compiledIngredient);
     };
 
-    $scope.addTechnique = function(){
-      console.log('clicked');
-      var nextTechnique = angular.element( document.querySelector( '.container-technique' ) );
-      console.log(nextTechnique);
-      var appendedTechnique = '<div class="input-group">';
-          appendedTechnique += '<input type="text" class="form-control" placeholder="12-inch non-stick Skillet">';
-          appendedTechnique += '<span class="input-group-btn">';
-          appendedTechnique += '<button class="btn btn-default" type="button" ng-click="addTechnique()">';
-          appendedTechnique += '<span class="glyphicon glyphicon-plus" aria-hidden="true">';
-          appendedTechnique += '</span></button></span>';
-          appendedTechnique += '</div>';
-        var compiledTechnique = $compile(appendedTechnique)($scope);
-        nextTechnique.append(compiledTechnique);
+    $scope.addUtensil = function(){
+      var nextUtensil = angular.element( document.querySelector( '.container-technique' ) );
+      console.log(nextUtensil);
+      var appendedUtensil = '<div class="input-group">';
+          appendedUtensil += '<input type="text" class="form-control utensilName" placeholder="12-inch non-stick Skillet">';
+          appendedUtensil += '<span class="input-group-btn">';
+          appendedUtensil += '<button class="btn btn-default" type="button" ng-click="addUtensil()">';
+          appendedUtensil += '<span class="glyphicon glyphicon-plus" aria-hidden="true">';
+          appendedUtensil += '</span></button></span>';
+          appendedUtensil += '</div>';
+        var compiledUtensil = $compile(appendedUtensil)($scope);
+        nextUtensil.append(compiledUtensil);
+    };
+
+    $scope.saveAndClear = function(){
+
+      var ingredientsArray = angular.element( document.querySelector( '.ingredient-name' ) ).val();
+
+      console.log("INGREDIENTSARRAY", ingredientsArray);
+
+      recipeRepoObj = {
+        recipeName: $scope.recipeName,
+        cuisineName: $scope.cuisineName,
+        cookingInstructions: $scope.cookingInstructions
+      };
+
+      console.log("RECIPEREPOOBJI", recipeRepoObj);
+
+    };
+
+    $scope.createToProfile = function(){
+
+    };
+
+    $scope.createToCulPort = function(){
+
     };
 
   }
